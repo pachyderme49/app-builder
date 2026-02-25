ee8d710aea07b619fee4273ad623b8b94721077d
pachyderme49 <263767152+pachyderme49@users.noreply.github.com>
2026-02-25 22:47:57 +0100

    fix(tests) : fix tests

--- apps/app-builder/src/app/app.spec.ts
+++ apps/app-builder/src/app/app.spec.ts
@@ -1,20 +1,25 @@
 import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { App } from './app';
-import { NxWelcome } from './nx-welcome';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App, NxWelcome],
+      imports: [App],
+      providers: [provideRouter([])],
     }).compileComponents();
   });
 
-  it('should render title', async () => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should have router-outlet', async () => {
+    const fixture = TestBed.createComponent(App);
+    await fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome app-builder'
-    );
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
